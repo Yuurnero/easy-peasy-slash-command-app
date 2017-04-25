e6901ee94d098e2ae4c00d816c70b8cd0df08aa4
Yolonero <s.lemaire28250@hotmail.com>
2017-04-26 03:27:35 +0900

    test tableflip

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+        case "/tableflip": //handle the `/echo` slash command. We might have others assigned to this app too!
             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
             // Otherwise just echo back to them what they sent us.
 
@@ -98,16 +98,15 @@ controller.on('slash_command', function (slashCommand, message) {
             // if no text was supplied, treat it as a help command
             if (message.text === "" || message.text === "help") {
                 slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
+                    "╯°□°)╯︵ ┻━┻");
                 return;
             }
 
             // If we made it here, just echo what the user typed back at them
             //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });
+            slashCommand.replyPublic(message, "╯°□°)╯︵ ┻━┻");//, function() {
+              //  slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+          //  });
 
             break;
         default:
@@ -117,4 +116,3 @@ controller.on('slash_command', function (slashCommand, message) {
 
 })
 ;
-
